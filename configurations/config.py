605118212b73f8744c94395100a244550cb53249
Sarthak1306 <sarthak13062001@gmail.com>
2023-07-26 18:04:16 +0530

    config files

---
+++ configurations/config.py
@@ -0,0 +1,3 @@
+# Change to FALSE if you need the app to just input 1 PDF File.
+
+allow_multiple_pdfs = True
---
+++ configurations/consts.py
@@ -0,0 +1,8 @@
+# Change the values of the variables according to your need
+
+page_title_field = "Chat with Multiple PDF's"
+page_icon_field = ":books:"
+page_header_field = "Chat with Multiple PDFs :books:"
+user_question_field = "What would you like to know about the pdf? :"
+sidebar_subheader_field = "Your Documents"
+file_uploader_field = "Upload your PDF Files"
